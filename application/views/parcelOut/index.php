0ad16a8494fb11e429f54ae5743d8d9e882b94c4
fyzh99 <fyzh99@naver.com>
2020-01-31 11:25:02 +0900

    setting

---
+++ application/views/parcelOut/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>apt</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="/public/css/front.css">
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+</nav>
+	
+<!-- start container -->
+<div class="container">
+	<div class="list">
+		<div class="row">
+		  <div class="col-xs-12">
+		  	<div><span class="label label-success">공공임대</span> <span class="label label-default">서울</span></div>
+		  	<p class="area">서울 주거 내용</p>
+		  </div>
+		</div>
+		<div class="row">
+		  <div class="col-xs-12">
+		  	<div><span class="label label-success">공공임대</span> <span class="label label-default">서울</span></div>
+		  	<p class="area">서울 주거 내용</p>
+		  </div>
+		</div>
+		<div class="row">
+		  <div class="col-xs-12">
+		  	<div><span class="label label-success">공공임대</span> <span class="label label-default">서울</span></div>
+		  	<p class="area">서울 주거 내용</p>
+		  </div>
+		</div>
+	</div>
+</div>
+<!-- end container -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
